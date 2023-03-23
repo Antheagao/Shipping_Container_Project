c4c74a3b4e7a6c5f510bc72f3041f1e311fa1ad4
Antheagao <Anthonymendez035@gmail.com>
2023-03-22 18:38:35 -0700

    working balance search for 4/5 cases.

--- final_project_v2.py
+++ final_project_v2.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from objects import Container, Ship
 from collections import defaultdict
+from collections import deque
 import heapq
 import copy
-from objects import Container, Ship
-        
+import time
 
+        
 def main():
     # Declare variables
     file_name = ''
@@ -13,11 +15,12 @@ def main():
     B_COLS = 24
     S_ROWS = 8
     S_COLS = 12
-    ship = [[Container('a', 0) for i in range(S_COLS)] for j in range(S_ROWS)]
-    buffer = [['   ' for i in range(B_COLS)] for j in range(B_ROWS)]
+    bay = [[Container('', 0) for i in range(S_COLS)] for j in range(S_ROWS)]
+    buffer = [[Container('', 0) for i in range(B_COLS)] for j in range(B_ROWS)]
     
     # Get the manifest file from the user
-    file_name = str(input('Enter the name of the manifest file: '))
+    '''file_name = str(input('Enter the name of the manifest file: '))'''
+    file_name = 'ShipCase5.txt'
     
     # Read the manifest file into a dataframe
     manifest = pd.read_csv(file_name, sep=',', header=None, 
@@ -27,14 +30,21 @@ def main():
     
     # Clean the dataframe
     clean_df(df)
-    print(df)
+    '''print(df)'''
     
     # Build the 2d table to represent the ship
-    build_ship(ship, S_ROWS, S_COLS, df)
-    print(ship[0][0].name)
+    build_ship(bay, S_ROWS, S_COLS, df)
     
     # Balance the ship
-    '''print(a_star(ship, df, manifest))'''
+    ship = Ship(bay, '', 0)
+    print('Original Ship')
+    print_table(ship.bay, S_COLS)
+    print('left weight: ', ship.get_left_kg(), 'right weight: ', ship.get_right_kg())
+    print('Balanced Ship')
+    time1 = time.perf_counter()
+    operations = a_star(ship, df, manifest)
+    time2 = time.perf_counter()
+    print('Time: ', time2 - time1)
     
     # Create the updated manifest file
     '''update_manifest(file_name, manifest)'''
@@ -70,107 +80,84 @@ def build_ship(ship : list[list[Container]], S_ROWS : int, S_COLS : int,
 
 def print_table(ship : list[list[Container]], COLS : int) -> None:
     # Declare variables
-    COL_WIDTH = 3
+    WIDTH = 3
     
     # Print the ship table with formatting and bars
-    print_bars(COLS, COL_WIDTH)
-    for row in ship:
-        for col in row:
-            print('| ', col.name[0:COL_WIDTH].ljust(COL_WIDTH), sep='', end=' ')
-        print('|')
-        print_bars(COLS, COL_WIDTH)
-
-
-def print_bars(COLS : int, COL_WIDTH : int) -> None:
-    # Print bars to divide the ship table into sections
-    for row in range(COLS * COL_WIDTH * 2 + 1):
+    for row in range(COLS * WIDTH * 2 + 1):
         print('-', end='')
     print()
-
-
-def is_balanced(ship : list[list[str]], S_ROWS : int, S_COLS : int,
-                df : pd.DataFrame) -> bool:
-    # Declare variables
-    left_kg = 0.0
-    right_kg = 0.0
-    manifest_index = 0
-    
-    # Sum the weight of both sides of the ship and divide min by max weight
-    for row in range(S_ROWS):
-        for col in range(S_COLS):
-            manifest_index = (S_ROWS - 1 - row) * S_COLS + col
-            if ship[row][col] == '+++' or ship[row][col] == '   ':
-                continue
-            elif (col < S_COLS // 2):
-                left_kg += df.iloc[manifest_index]['Weight']
-            else:
-                right_kg += df.iloc[manifest_index]['Weight']
-
-    # Return true if the weight is balanced and false if not
-    return min(left_kg, right_kg) / max(left_kg, right_kg) > 0.9
+    for row in ship:
+        for col in row:
+            print('| ', col.name[0:WIDTH].ljust(WIDTH), sep='', end=' ')
+        print('|')
+        for row in range(COLS * WIDTH * 2 + 1):
+            print('-', end='')
+        print()
                 
 
-def a_star(start : list[list[str]], df : pd.DataFrame,
-           manifest : pd.DataFrame) -> None:
+def a_star(start : Ship, df : pd.DataFrame, manifest : pd.DataFrame) -> None:
     # Declare variables
-    S_ROWS = len(start)
-    S_COLS = len(start[0])
+    S_ROWS = len(start.bay)
+    S_COLS = len(start.bay[0])
     can_drop_off = False
     open_set = []
     states = []
     seen = set()
-    ship = Ship(start, '', 0)
-    heapq.heappush(open_set, ship)
+    heapq.heappush(open_set, start)
     came_from = {}
+    start_hash = (start.get_hash(), start.last_held)
     g_score = defaultdict(lambda: float('inf'))
-    g_score[str((ship.bay, ship.last_held))] = 0
+    g_score[start_hash] = 0
     f_score = defaultdict(lambda: float('inf'))
-    f_score[str((ship.bay, ship.last_held))] = heuristic(ship, df)
+    f_score[start_hash] = heuristic(start)
     
     # Find the shortest path to a balanced ship if it exists
     while len(open_set) > 0:
-        current = heapq.heappop(open_set)
-        seen.add(str((ship.bay, ship.last_held)))
-        if is_balanced(ship.bay, S_ROWS, S_COLS, df):
-            return reconstruct_path(came_from, str((ship.bay, ship.last_held)))
+        ship = heapq.heappop(open_set)
+        ship_hash = (ship.get_hash(), ship.last_held)
+        seen.add(ship_hash)
+        '''print_table(ship.bay, S_COLS)
+        pause = input('Press enter to continue...')'''
+        if ship.is_balanced():
+            print_table(ship.bay, S_COLS)
+            print('left weight: ', ship.get_left_kg(), 'right weight: ', ship.get_right_kg())
+            return create_path(came_from, ship_hash)
 
         # Check if the ship is in a state where it can pick up or drop off
         if can_drop_off == False:
-            states = expand_pick_up(current, df, seen, S_ROWS, S_COLS)
+            states = expand_pick_up(ship, seen, S_ROWS, S_COLS)
             can_drop_off = True
         else:
-            states = expand_drop_off(current, df, seen, S_ROWS, S_COLS)
+            states = expand_drop_off(ship, seen, S_ROWS, S_COLS)
             can_drop_off = False
         for state in states:
-            current_hash = str((current.bay, current.last_held))
-            neighbor_hash = str((state.bay, state.last_held))
-            temp_g_score = g_score[current_hash] + state.cost
+            neighbor_hash = (state.get_hash(), state.last_held)
+            temp_g_score = g_score[ship_hash] + state.cost
             if temp_g_score < g_score[neighbor_hash]:
-                came_from[neighbor_hash] = current_hash
+                came_from[neighbor_hash] = ship_hash
                 g_score[neighbor_hash] = temp_g_score
-                f_score[neighbor_hash] = temp_g_score + heuristic(state, df)
-                if state not in open_set:
+                f_score[neighbor_hash] = temp_g_score + heuristic(state)
+                if neighbor_hash not in open_set:
                     heapq.heappush(open_set, state)
                     
     # Ship cannot be balanced, perform SIFT
     return 'failure'
  
  
-def reconstruct_path(came_from : dict, current : str) -> list[str]:
-    total_path = [current]
+def create_path(came_from : dict, current : tuple[str, str]) -> list[str]:
+    print('success')
+    total_path = deque(current)
     while current in came_from:
         current = came_from[current]
-        total_path.prepend(current)
+        total_path.appendleft(current)
     return total_path
  
  
-def heuristic(ship : Ship, df : pd.DataFrame) -> int:
+def heuristic(ship : Ship) -> int:
     # Declare variables
-    S_ROWS = len(ship.bay)
     S_COLS = len(ship.bay[0])
-    left_kg = 0.0
-    right_kg = 0.0
-    manifest_index = 0
+    left_kg = ship.get_left_kg()
+    right_kg = ship.get_right_kg()
     balance_mass = 0
     deficit = 0
     masses = []
@@ -178,37 +165,14 @@ def heuristic(ship : Ship, df : pd.DataFrame) -> int:
     items = []
     
     # Use left and right weight to calculate the balance mass and deficit
-    for row in range(S_ROWS):
-        for col in range(S_COLS):
-            manifest_index = (S_ROWS - 1 - row) * S_COLS + col
-            if ship.bay[row][col] == '+++' or ship.bay[row][col] == '   ':
-                continue
-            elif (col < S_COLS // 2):
-                left_kg += df.iloc[manifest_index]['Weight']
-            else:
-                right_kg += df.iloc[manifest_index]['Weight']
     balance_mass = (left_kg + right_kg) / 2 
     deficit = balance_mass - min(left_kg, right_kg)
     
     # Make a list of the masses on the smaller side of the ship
     if min(left_kg, right_kg) == right_kg:
-        for row in range(S_ROWS):
-            for col in range(S_COLS // 2):
-                if ship.bay[row][col] == '+++' or ship.bay[row][col] == '   ':
-                    continue
-                manifest_index = (S_ROWS - 1 - row) * S_COLS + col
-                name = df.iloc[manifest_index]['Weight']
-                temp = (ship.bay[row][col], name, col)
-                masses.append(temp)
+        masses = ship.get_left_containers()
     else:
-        for row in range(S_ROWS):
-            for col in range(S_COLS // 2, S_COLS):
-                if ship.bay[row][col] == '+++' or ship.bay[row][col] == '   ':
-                    continue
-                manifest_index = (S_ROWS - 1 - row) * S_COLS + col
-                name = df.iloc[manifest_index]['Weight']
-                temp = (ship.bay[row][col], name, col)
-                masses.append(temp)
+        masses = ship.get_right_containers()
     
     # Sort the masses in decending order by weight
     masses = sorted(masses, key=lambda x: x[1], reverse=True)
@@ -231,55 +195,54 @@ def heuristic(ship : Ship, df : pd.DataFrame) -> int:
     return h_n
 
 
-def expand_pick_up(ship : Ship, df : pd.DataFrame, seen : set, 
+def expand_pick_up(ship : Ship, seen : set, 
                    S_ROWS : int, S_COLS : int) -> list[Ship]:
     # Declare variables
     states = []
-    state = ()
     cost = 0
     
     # Store the state of top containers that can be picked up in each column
     for col in range(S_COLS):
         for row in range(S_ROWS):
-            manifest_index = (S_ROWS - 1 - row) * S_COLS + col
-            temp_ship = copy.deepcopy(ship.bay)
-            state = (temp_ship, df.iloc[manifest_index]['Name'])
-            if state[1] == 'NAN' or state[1] == 'UNUSED' or str(state) in seen:
+            bay = copy.deepcopy(ship.bay)
+            hold = bay[row][col].name
+            if hold == '+++' or hold == '   ' or str((bay, hold)) in seen:
                 continue
             else:
+                # Calculate the cost and store the ship
                 cost = abs(-1 - row) + abs(0 - col)
-                new_ship = Ship(state[0], state[1], cost)
-                states.append(new_ship)
+                states.append(Ship(bay, hold, cost))
                 break
     return states
                
     
-def expand_drop_off(ship : Ship, df : pd.DataFrame, seen : set,
+def expand_drop_off(ship : Ship, seen : set,
                     S_ROWS : int, S_COLS : int) -> list[Ship]:
     # Declare variables
     states = []
-    state = ()
     
     # Store the state of containers that can be dropped off in top empty cells
     for col in range(S_COLS):
         for row in reversed(range(S_ROWS)):
-            manifest_index = (S_ROWS - 1 - row) * S_COLS + col
-            temp_ship = copy.deepcopy(ship.bay)
-            state = (temp_ship, df.iloc[manifest_index]['Name'])
-            if state[1] == ship.last_held:
+            bay = copy.deepcopy(ship.bay)
+            hold = bay[row][col].name
+            if hold == ship.last_held:
                 break
-            if state[1] == 'NAN' or state[1] != 'UNUSED' or str(state) in seen:
+            if hold == '+++' or hold != '   ' or str((bay, hold)) in seen:
                 continue
             else:
-                move = np.argwhere(np.array(state[0]) == ship.last_held)
-                x = move[0][0]
-                y = move[0][1]
-                state[0][row][col], state[0][x][y] = \
-                    state[0][x][y], state[0][row][col]
+                # Get the coordinates of the last held container
+                x, y = ship.get_coordinates(ship.last_held)
+                
+                # Swap the last held container with the empty cell
+                bay[row][col], bay[x][y] = bay[x][y], bay[row][col]
+                
+                # Calculate the cost of the new state
                 cost_to_top = abs(x - -1) 
                 cost = cost_to_top + abs(-1 - row) + abs(y - col)
-                new_ship = Ship(state[0], ship.last_held, cost)
-                states.append(new_ship)
+                
+                # Create the new ship
+                states.append(Ship(bay, ship.last_held, cost))
                 break
     return states
                 
@@ -289,4 +252,5 @@ def update_manifest(file_name : str, manifest : pd.DataFrame) -> None:
     manifest.to_csv(file_name, header=None, index=False)
     
 
-main()
+if __name__ == '__main__':
+    main()
